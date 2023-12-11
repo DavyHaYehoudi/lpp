801ec559ee8178bebdae364445e2918c2f8711f2
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-12-11 01:29:54 +0100

    implementation general search bar

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,39 +1,47 @@
 import { recipes } from "../data/recipes.js";
 import { card } from "./card.js";
+import { searchRecipes } from "./searchGeneral.js";
 
 const $cards = document.querySelector("#cards");
+const $recipesNumberFind = document.querySelector("#recipesNumberFind");
 
 export const cardsList = (filterCriteria = {}, limit = 10) => {
-  let filteredRecipes = [...recipes];
-
-  if (filterCriteria && Object.keys(filterCriteria).length > 0) {
-    filteredRecipes = filteredRecipes.filter((item) => {
-      // Appliquer les critères de filtrage ici
-      // Exemple: vérifier si chaque critère correspond à la propriété de la recette
-      return Object.entries(filterCriteria).every(
-        ([key, value]) => item[key] === value
-      );
-    });
+  let filteredRecipes;
+  if (
+    filterCriteria?.searchGeneral &&
+    filterCriteria.searchGeneral.length >=3
+  ) {
+    // Utiliser la fonction de recherche si une requête de recherche générale est présente
+    filteredRecipes = searchRecipes(filterCriteria.searchGeneral, recipes);
+    $recipesNumberFind.textContent=`${filteredRecipes.length} recette${filteredRecipes.length>1?"s":""}`
+  } else {
+    // Utiliser la logique de filtrage par défaut si aucune recherche générale
+    filteredRecipes = [...recipes];
+    $recipesNumberFind.textContent=`${limit} recettes`
   }
 
   $cards.innerHTML = "";
-
-  filteredRecipes.slice(0, limit).forEach((item) => {
+  const slicedRecipes = filterCriteria.searchGeneral
+    ? filteredRecipes
+    : filteredRecipes.slice(0, limit);
+  slicedRecipes.forEach((item) => {
     const { image, name, time, description, ingredients } = item;
-  
+
     const ingredientList = ingredients.map((el) => {
-      const { ingredient, quantity = '', unit = '' } = el;
+      const { ingredient, quantity = "", unit = "" } = el;
       return { ingredient, quantity, unit };
     });
-  
-    const ingredientsData = ingredientList.reduce((acc, el) => {
-      acc.ingredients.push(el.ingredient);
-      acc.quantities.push(el.quantity);
-      acc.units.push(el.unit);
-      return acc;
-    }, { ingredients: [], quantities: [], units: [] });
-  
-  
+
+    const ingredientsData = ingredientList.reduce(
+      (acc, el) => {
+        acc.ingredients.push(el.ingredient);
+        acc.quantities.push(el.quantity);
+        acc.units.push(el.unit);
+        return acc;
+      },
+      { ingredients: [], quantities: [], units: [] }
+    );
+
     $cards.innerHTML += card(
       image,
       name,
@@ -44,5 +52,4 @@ export const cardsList = (filterCriteria = {}, limit = 10) => {
       ingredientsData.units
     );
   });
-  
 };
---
+++ scripts/searchGeneral.js
@@ -0,0 +1,43 @@
+import { cardsList } from "./cards.js";
+
+export const handleSearchGeneralInput = () => {
+  const searchTerm = document.getElementById("search-bar").value;
+  cardsList({ searchGeneral: searchTerm });
+};
+
+export const setupSearchEvent = () => {
+    const $searchForm = document.getElementById("searchGeneralForm");
+    $searchForm.addEventListener("submit", function (event) {
+      handleSearchGeneralInput();
+    });
+  };
+  
+
+export const searchRecipes = (query, recipes) => {
+  const results = [];
+
+  // Convertir la requête en minuscules pour une recherche insensible à la casse
+  const queryLower = query.toLowerCase();
+
+  // Filtrer les recettes qui correspondent à la requête dans le titre, les ingrédients ou la description
+  recipes.forEach((recipe) => {
+    const { name, ingredients, description } = recipe;
+    const recipeLower = name.toLowerCase();
+    const ingredientsLower = ingredients.map((ing) =>
+      ing.ingredient.toLowerCase()
+    );
+    const descriptionLower = description.toLowerCase();
+
+    // Vérifier la correspondance dans le titre, les ingrédients ou la description
+    if (
+      recipeLower.includes(queryLower) ||
+      ingredientsLower.some((ing) => ing.includes(queryLower)) ||
+      descriptionLower.includes(queryLower)
+    ) {
+      results.push(recipe);
+    }
+  });
+
+  console.log('results:', results)
+  return results;
+};
