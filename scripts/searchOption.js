ec5148d97809605a1b781169a930f953cebe15cc
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-12-14 12:49:34 +0100

    filtrage options en fonction texte recherche

---
+++ scripts/searchOption.js
@@ -0,0 +1,42 @@
+import { getOptionsList } from "./optionsList.js";
+import { customListOptions } from "./tags.js";
+
+const $searchOptionInput = document.querySelectorAll(".search-bar-select");
+const sections = {
+  ingredients: "#ingredients_listBox > div.listbox-content > ul.options li",
+  appliance: "#appliance_listBox > div.listbox-content > ul.options li",
+  ustensils: "#ustensils_listBox > div.listbox-content > ul.options li",
+};
+
+let options = {
+  ingredients: [],
+  appliance: [],
+  ustensils: [],
+};
+
+$searchOptionInput.forEach((input) => { //Appliquer pour tous les inputs de la classe sélectionnée
+  input.addEventListener("input", (event) => { //Attribuer un événement 
+    for (const [key, selector] of Object.entries(sections)) {
+      const section = document.querySelectorAll(selector);
+
+      section.forEach((el) => {
+        const optionName = el.getAttribute("data-name");
+        options = { ...options, [key]: [...options[key], optionName] };//Alimenter l'objet options par ce qui est contenu dans les listes
+      });
+    }
+
+    const optionsFilter = { //Modifier les valeurs en fonction du texte saisi dans l'input
+      ...options,
+      [event.target.name]: [
+        ...new Set(
+          options[event.target.name].filter((item) =>
+            item.toLowerCase().includes(event.target.value.toLowerCase())
+          )
+        ),
+      ],
+    };
+
+    getOptionsList(optionsFilter);
+    customListOptions()
+  });
+});
