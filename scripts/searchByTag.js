94ccc281590963ec0d7c68bb57a940d75af9ea75
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-12-17 14:15:50 +0100

    suppression tag sans recherche principale + refactorisation algo

--- scripts/searchByTag.js
+++ scripts/searchByTag.js
@@ -2,55 +2,65 @@ import { recipes } from "../data/recipes.js";
 import { searchRecipes } from "./searchRecipes.js";
 import { updateRecipes } from "./utils.js";
 
-export const searchByTag = (action, dataName) => {
+//Récupération des id des recettes affichées
+const getSelectedIds = () => {
   const articles = document.querySelectorAll("#cards > article");
+  return Array.from(articles, (article) => article.getAttribute("data-id"));
+};
 
-  let recipesSelectedId = [];
-  let filteredRecipes;
-  const storedCriteria = localStorage.getItem("searchCriteria");
-
-  articles.forEach((article) => {
-    const id = article.getAttribute("data-id");
-    recipesSelectedId.push(id);
-  });
-
+const filterByTag = (action, dataName, recipesSelectedId, storedCriteria) => {
   if (action === "add") {
     const searchResults = searchRecipes("tag", recipes, dataName);
-    filteredRecipes = searchResults.filter((item) =>
+    return searchResults.filter((item) =>
       recipesSelectedId.includes(String(item.id))
     );
-    updateRecipes(filteredRecipes);
   } else if (action === "delete") {
-    if (storedCriteria) {
-      const searchMain = searchRecipes(storedCriteria, recipes);
-      const activeRecipeDataNames = Array.from(
-        document.querySelectorAll("#tags li")
-      )
-        .map((tag) => tag.getAttribute("data-name"))
-        .filter((name) => name !== dataName);
+    //Inventaire des tags actifs après la suppression de l'un d'entre-eux
+    const activeRecipeDataNames = Array.from(
+      document.querySelectorAll("#tags li")
+    )
+      .map((tag) => tag.getAttribute("data-name"))
+      .filter((name) => name !== dataName);
 
-      if (activeRecipeDataNames.length > 0) {
-        const searchResults = activeRecipeDataNames.map((name) =>
-          searchRecipes("tag", recipes, name)
-        );
+    //S'il reste encore au moins un tag après la suppression de l'un d'entre-eux
+    if (activeRecipeDataNames.length > 0) {
+      const searchResults = activeRecipeDataNames.map((name) =>
+        searchRecipes("tag", recipes, name)
+      );
+      const commonResults = searchResults.reduce(
+        (acc, currentArray) =>
+          acc.filter((recipe) => currentArray.some((r) => r.id === recipe.id)),
+        searchResults[0]
+      );
 
-        const commonResults = searchResults.reduce((acc, currentArray) => {
-          return acc.filter((recipe) =>
-            currentArray.some((r) => r.id === recipe.id)
-          );
-        }, searchResults[0]);
-
-        filteredRecipes = commonResults.filter((result) =>
+      if (storedCriteria) {
+        //Une recherche principale est à considérer
+        const searchMain = searchRecipes(storedCriteria, recipes);
+        return commonResults.filter((result) =>
           searchMain.some((sm) => sm.id === result.id)
         );
-      } else if (storedCriteria) {
-        //Une recherche générale est-elle à prendre en compte ?
-        filteredRecipes = searchRecipes(storedCriteria, recipes);
       } else {
-        //Uniquement recherche par tag
-        filteredRecipes = [...recipes];
+        //Ce n'est qu'une recherche à partir des tags
+        return commonResults;
       }
+      //Suppression du dernier tag
+    } else if (storedCriteria) {
+      return searchRecipes(storedCriteria, recipes);
+    } else {
+      return [...recipes];
     }
-    updateRecipes(filteredRecipes);
   }
+  return [];
+};
+
+export const searchByTag = (action, dataName) => {
+  const recipesSelectedId = getSelectedIds();
+  const storedCriteria = localStorage.getItem("searchCriteria");
+  const filteredRecipes = filterByTag(
+    action,
+    dataName,
+    recipesSelectedId,
+    storedCriteria
+  );
+  updateRecipes(filteredRecipes);
 };
